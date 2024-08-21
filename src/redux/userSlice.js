5a8cda64eec0a9a4d69c339cd487ee7de84c6625
KrushilProlink <138547321+KrushilProlink@users.noreply.github.com>
2024-08-21 18:48:35 +0530

    Update userSlice.js

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -2,37 +2,37 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
   {
-    "id": "1",
+    "id": 1,
     "userName": "john_doe",
     "email": "john.doe@example.com",
     "role": "user"
   },
   {
-    "id": "2",
+    "id": 2,
     "userName": "jane_smith",
     "email": "jane.smith@example.com",
     "role": "admin"
   },
   {
-    "id": "3",
+    "id": 3,
     "userName": "bob_jackson",
     "email": "bob.jackson@example.com",
     "role": "user"
   },
   {
-    "id": "4",
+    "id": 4,
     "userName": "alice_jones",
     "email": "alice.jones@example.com",
     "role": "user"
   },
   {
-    "id": "5",
+    "id": 5,
     "userName": "david_miller",
     "email": "david.miller@example.com",
     "role": "admin"
   },
   {
-    "id": "6",
+    "id": 6,
     "userName": "emily_carter",
     "email": "emily.carter@example.com",
     "role": "user"
@@ -44,25 +44,25 @@ const initialState = [
     "role": "user"
   },
   {
-    "id": "8",
+    "id": 8,
     "userName": "olivia_baker",
     "email": "olivia.baker@example.com",
     "role": "editor"
   },
   {
-    "id": "9",
+    "id": 9,
     "userName": "samuel_clark",
     "email": "samuel.clark@example.com",
     "role": "admin"
   },
   {
-    "id": "10",
+    "id": 10,
     "userName": "victoria_smith",
     "email": "victoria.smith@example.com",
     "role": "user"
   },
   {
-    "id": "11",
+    "id": 11,
     "userName": "zoe_campbell",
     "email": "zoe.campbell@example.com",
     "role": "admin"
